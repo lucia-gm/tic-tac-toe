82e1c1334913aab1b734c6c1cf8db273e671d68d
Lucia Gonzalez Moscoso <luciagonzalezmoscoso@gmail.com>
2018-10-13 22:11:32 -0700

    Updates

---
+++ precache-manifest.489d52cfe5d189ea9f125568782c5d73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "0fbd9dc1029a01974465",
+    "url": "/tic-tac-toe/static/js/main.0fbd9dc1.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/tic-tac-toe/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "0fbd9dc1029a01974465",
+    "url": "/tic-tac-toe/static/css/main.526c7e74.chunk.css"
+  },
+  {
+    "revision": "a57b0bb8cd7a9f9879d0d4576a605413",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
